25b8f120f52031deafe6844048f53140a78cddce
pekachoo <jliu6162@gmail.com>
2024-03-27 11:56:32 -0400

    derivative and cost function

--- LogisticClassificationModel.py
+++ LogisticClassificationModel.py
@@ -6,7 +6,27 @@ import pandas as pd
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def cost_function()
+
+def cost_function(X, y, w, b):
+    # size of data set X, containing multiple features
+    m = X.shape[0]
+    z = np.dot(X, w) + b
+    f_wbx = sigmoid(z)
+    total_cost = (-1 / m) * np.sum(y * np.log(f_wbx) + (1 - y) * np.log(1 - f_wbx))
+    return total_cost
+
+
+def calculate_derivatives(X, y, w, b):
+    m = X.shape[0]
+    z = np.dot(X, w) + b
+    f_wbx = sigmoid(z)
+    dw = 0
+    for i in range(m):
+        dw += (f_wbx[i] - y[i]) * X[i]
+
+    db = (1 / m) * np.sum(h - y)
+    return dw, db
+
 
 data = pd.read_csv('bmd.csv')
 
